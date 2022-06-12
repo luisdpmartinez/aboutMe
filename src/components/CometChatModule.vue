432a4723784bd2ea1e27a3104c9cdc23397935bb
Luis Daniel Puente Martinez <lmartinez@ampliostrategies.com>
2022-06-12 12:54:53 -0400

    basic layout done

---
+++ src/components/CometChatModule.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page style="height: 100%" id="callScreen">
+    <q-footer
+      flat
+      :class="$q.dark.isActive ? 'bg-black text-light' : 'bg-white text-black'"
+    >
+      <div :class="`q-pa-md row justify-center`">
+        <q-btn-dropdown split rounded :icon="'videocam'" class="q-mx-sm">
+        </q-btn-dropdown>
+        <q-btn-dropdown split rounded :icon="'mic'" class="q-mx-sm">
+        </q-btn-dropdown>
+        <q-btn
+          @click="$q.dark.toggle()"
+          :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'"
+          rounded
+          class="q-mx-sm"
+        >
+        </q-btn>
+        <q-btn flat icon="call_end" color="red" @click="router.push('/')" />
+      </div> </q-footer
+  ></q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'CometChat',
+  setup() {
+    const router = useRouter();
+    return {
+      router,
+    };
+  },
+});
+</script>
--- src/pages/VideoCallPage.vue
+++ src/pages/VideoCallPage.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page> test </q-page>
+  <q-page>
+    <q-card :style="`height: ${height - 68}px`" flat>
+      <comet-chat-module />
+    </q-card>
+  </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import CometChatModule from 'src/components/CometChatModule.vue';
+import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
   name: 'VideoCallPage',
+  components: { CometChatModule },
+  setup() {
+    const height = ref(0);
+
+    function onResize() {
+      height.value = window.innerHeight;
+    }
+    onMounted(() => {
+      height.value = window.innerHeight;
+      window.addEventListener('resize', onResize);
+    });
+    return {
+      height,
+    };
+  },
 });
 </script>
