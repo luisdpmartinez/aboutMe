ed2944400439b3704871e779ca6c346bf9f21206
Luis Daniel Puente Martinez <luisdpmartinez@icloud.com>
2022-07-02 19:06:46 -0400

    another try

--- apollo.config.js
+++ apollo.config.js
@@ -3,8 +3,8 @@
 module.exports = {
   client: {
     service: {
-      name: 'my-service',
-      url: 'http://localhost:3000/graphql',
+      name: 'strapi',
+      url: 'import.meta.env.VITE_STRAPI_URL',
     },
     // Files processed by the extension
     includes: ['src/**/*.vue', 'src/**/*.js', 'src/**/*.ts'],
